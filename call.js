e24f8c907081cdda11445b47b48bb8c483bda627
Aisha Anvi <anviaisha6509@gmail.com>
2025-08-29 20:06:48 +0600

    completed call center perfectly

--- call.js
+++ call.js
@@ -31,3 +31,51 @@ cardButton.forEach((button, index) => {
         });
     });
 });
+
+
+// call list
+
+const callButton =document.querySelectorAll(".calling-button")
+const coinDiv =document.getElementById("coin")
+const callHistory =document.getElementById("recorder-card")
+const clearBtn =document.getElementById("clear-button")
+
+callButton.forEach (button=>{
+    button.addEventListener("click",() => {
+        
+        let coins =parseInt(coinDiv.textContent);
+        if (coins >= 20){
+            coins-=20;
+            coinDiv.textContent=coins;
+            alert("calling...")
+            const time=new Date();
+            const timeString= time.toLocaleTimeString();
+            const newCallDiv=document.createElement('div');
+            const card =button.closest(".card")
+            const title= card.querySelector(".card-name").innerText;
+            const Number=card.querySelector(".card-num").innerText;
+
+            newCallDiv.className = "bg-[#eeebeb] m-3 p-2 justify-between rounded-sm mb-2";
+            newCallDiv.innerHTML=`
+            <div> 
+             <div> 
+                <h3 class="font-bold">${title}</h3> 
+                <p class="text-gray-400">${Number}</p> 
+             </div>
+             <div> 
+                <p>${timeString}</p> 
+             </div> 
+            </div>`;
+            callHistory.appendChild(newCallDiv);
+        }
+        else{
+            alert("Not enough coins 😔!!")
+        }
+    })
+})
+
+// clear button
+
+clearBtn.addEventListener("click",() =>{
+    callHistory.innerHTML="";
+});
